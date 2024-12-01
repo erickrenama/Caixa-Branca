290db99f90d45f7c0b39d025ab176e7eb728611e
Erick <erickrenan.567@gmail.com>
2024-12-01 19:47:05 -0300

    first commit

---
+++ Etapa 3/README.md
@@ -0,0 +1,26 @@
+![alt text](image.png)
+
+Complexidade Ciclomática
+A complexidade ciclomática do código é calculada como 3, conforme a seguinte fórmula:
+
+[V(G) = E - N + 2P]
+
+Onde:
+
+E: 12
+N: 11
+P: 1
+Com isso, temos que a complexidade ciclomática do código é 3
+
+Identificamos os seguintes caminhos independentes no código:
+
+Caminho 1:
+N1 → N5 → N6 → N7 → N8 → N9 → N11
+
+
+Caminho 2:
+N1 → N5 → N6 → N7 → N10 → N11
+
+
+Caminho 3:
+N1 → N5 → N6 → N7 → N8 → N10 → N11
---
+++ Etapa 3/User.java
@@ -0,0 +1,48 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class User {
+    // Ponto 1: Início
+    public Connection conectarBD() {
+        Connection conn = null;
+        try {
+            // Ponto 2: Conecta ao banco de dados
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
+            conn = DriverManager.getConnection(url);
+        } catch (Exception e) {
+            // Ponto 3: Exceção de conexão
+        }
+        return conn; // Ponto 4: Retorna (null ou válida)
+    }
+
+    public String nome = "";
+    public boolean result = false;
+
+    public boolean verificarUsuario(String login, String senha) {
+        String sql = "";
+        Connection conn = conectarBD(); // Ponto 5: Chama conectarBancodeDados()
+
+        // Ponto 6: query SQL
+        sql += "SELECT nome FROM usuarios ";
+        sql += "WHERE login = '" + login + "' ";
+        sql += "AND senha = '" + senha + "'";
+
+        try {
+            // Ponto 7: Criação de Statement e execução da query
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            // Ponto 8: Verifica se a query retornou resultados
+            if (rs.next()) {
+                result = true;
+                nome = rs.getString("nome"); // Ponto 9: Atualiza variáveis
+            }
+        } catch (Exception e) {
+            // Ponto 10: Tratamento de exceção
+        }
+        return result; // Ponto 11: Retorna o resultado
+    }
+}
